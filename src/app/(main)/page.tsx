09c1441e20cf33f2b501363ce529ee4405475c18
LGY <leegyoung12@gmail.com>
2024-11-27 05:33:53 +0900

    ✨Feat : svg 시범 추가
    - 8개 svg에 대해 시범적 추가
    - unexpected token 에러 발생함

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,15 @@
 import LoadingSpinner from "@/components/spinner";
+import heart_pressed from "@/assets/icons/heart_pressed.svg";
 
 export default function HomePage() {
   return (
     <section>
       <h2 className="font-bold text-2xl underline">Homepage</h2>
       <LoadingSpinner />
+      {/* 뜨지 않음 */}
+      <heart_pressed className="w-8 h-8" />
+      {/* Unexpected token 에러 */}
+      <img src={heart_pressed} alt="heart" className="w-8 h-8" />
     </section>
   );
 }
