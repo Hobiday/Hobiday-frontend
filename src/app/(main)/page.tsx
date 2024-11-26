1726bf6cd4cb4d905a920a6c4ab7f8715e2335b8
LGY <leegyoung12@gmail.com>
2024-11-27 05:33:53 +0900

    ✨Feat : svg 시범 추가
    - 8개 svg에 대해 시범적 추가
    - unexpected token 에러 발생함

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { MainLayout } from "@/components/layout";
 import AdBanner from "./_components/banner";
 import PerformanceList from "./_components/performance-list";
@@ -6,6 +7,10 @@ import Search from "@/assets/icons/search.svg";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import TextLogo from "../../../public/img/logo-text.svg";
+=======
+import LoadingSpinner from "@/components/spinner";
+import heart_pressed from "@/assets/icons/heart_pressed.svg";
+>>>>>>> 09c1441 (✨Feat : svg 시범 추가)
 
 export default function HomePage() {
   const headerProps = {
@@ -20,9 +25,20 @@ export default function HomePage() {
   }
 
   return (
+<<<<<<< HEAD
     <MainLayout headerProps={headerProps}>
       <AdBanner />
       <PerformanceList />
     </MainLayout>
+=======
+    <section>
+      <h2 className="font-bold text-2xl underline">Homepage</h2>
+      <LoadingSpinner />
+      {/* 뜨지 않음 */}
+      <heart_pressed className="w-8 h-8" />
+      {/* Unexpected token 에러 */}
+      <img src={heart_pressed} alt="heart" className="w-8 h-8" />
+    </section>
+>>>>>>> 09c1441 (✨Feat : svg 시범 추가)
   );
 }
