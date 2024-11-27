9f95c88300a7e6ee9bdee7d95ec39dada5ebe709
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-11-28 07:14:53 +0900

    ✨ Feat: 공통 컴포넌트(button) 제작

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,32 @@
+"use client";
+
+import { Button } from "@/components/button/button";
 import LoadingSpinner from "@/components/spinner";
-import HomeDefault from "@/assets/icons/home-default.svg";
 
 export default function HomePage() {
   return (
     <section>
       <h2 className="font-bold text-2xl underline">Homepage</h2>
       <LoadingSpinner />
-
-      <HomeDefault />
-      <HomeDefault className="fill-blue-300" />
+      {/* Button 테스트 */}
+      <div className="space-y-4 mt-6">
+        <Button variant="filled" size="sm" type="button" onClick={() => alert("Small Button Clicked!")}>
+          Small Button
+        </Button>
+        <Button variant="filled" size="md">
+          Medium Button
+        </Button>
+        <Button variant="filled" size="lg" className="bg-blue-500 hover:bg-blue-600">
+          Large Button
+        </Button>
+        <Button variant="filled" size="md" disabled>
+          Disabled Button
+        </Button>
+        <div className="bg-gray-200 w-50 h-50">hi</div>
+        <Button variant="filled" size="md" fullWidth>
+          Full-Width Button
+        </Button>
+      </div>
     </section>
   );
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,55 @@
-import { cva } from "class-variance-authority";
+import cn from "@/lib/tailwind-cn";
+import { cva, VariantProps } from "class-variance-authority";
+import { ComponentProps } from "react";
 
-export default function button() {
-  const buttonVariants = cva(``, {
-    variants: {},
-  });
-  return <div></div>;
+type buttonVariant = "filled" | "text" | "elevated";
+type buttonSize = "sm" | "md" | "lg";
+
+const buttonVariants = cva(`inline-flex items-center justify-center rounded-lg font-semibold`, {
+  variants: {
+    variant: {
+      filled: "bg-primary text-white hover:bg-blue-300 active:bg-blue-500",
+    },
+    size: {
+      sm: "px-4 py-[6px] text-xs",
+      md: "px-6 py-[10px] text-sm",
+      lg: "px-8 py-[14px] text-lg",
+    },
+    fullWidth: {
+      true: "w-full",
+    },
+    disabled: {
+      true: "cursor-not-allowed ",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: "filled",
+      disabled: true,
+      className: "bg-gray-200 text-opacity-[0.38] hover:bg-gray-200 active:bg-gray-200",
+    },
+  ],
+  defaultVariants: {
+    variant: "filled",
+    size: "md",
+  },
+});
+
+type ButtonVariantProps = VariantProps<typeof buttonVariants>;
+
+type HTMLButtonProps = Pick<ComponentProps<"button">, "onClick" | "disabled" | "type" | "className" | "children">;
+
+type buttonProps = Omit<ButtonVariantProps, "theme" | "size"> &
+  HTMLButtonProps & {
+    variant: buttonVariant;
+    size?: buttonSize;
+  };
+
+export function Button({ variant, size, fullWidth, disabled, children, className, ...props }: buttonProps) {
+  const buttonClassName = buttonVariants({ variant, size, fullWidth, disabled });
+  return (
+    <button className={cn(buttonClassName, className)} {...props}>
+      {children}
+    </button>
+  );
 }
