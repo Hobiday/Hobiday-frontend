98f80297171d76778f5b6920290b1a2d5948fff5
LGY <leegyoung12@gmail.com>
2024-11-28 19:19:43 +0900

    ✨ Feat: svgr 예시 코드 작성 및 종속성 업데이트
    - main 페이지에 svg 컴포넌트 적용한 예시 코드 작성
    - svgr 관련 라이브러리와 종속성 버전 업데이트

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
 import LoadingSpinner from "@/components/spinner";
+import HomeDefault from "@/assets/icons/home-default.svg";
 
 export default function HomePage() {
   return (
     <section>
       <h2 className="font-bold text-2xl underline">Homepage</h2>
       <LoadingSpinner />
+
+      <HomeDefault />
+      <HomeDefault className="fill-blue-300" />
     </section>
   );
 }
