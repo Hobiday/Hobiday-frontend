2a123fb5ee8ce34a100fbfa8e459f0b6a823f1f8
LGY <leegyoung12@gmail.com>
2024-11-28 19:19:43 +0900

    ✨ Feat: svgr 예시 코드 작성 및 종속성 업데이트
    - main 페이지에 svg 컴포넌트 적용한 예시 코드 작성
    - svgr 관련 라이브러리와 종속성 버전 업데이트

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import { MainLayout } from "@/components/layout";
-import AdBanner from "./_components/banner";
-import PerformanceList from "./_components/performance-list";
-
 import Search from "@/assets/icons/search.svg";
+import { MainLayout } from "@/components/layout";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import TextLogo from "../../../public/img/logo-text.svg";
-=======
-import LoadingSpinner from "@/components/spinner";
-<<<<<<< HEAD
-import heart_pressed from "@/assets/icons/heart_pressed.svg";
->>>>>>> 09c1441 (✨Feat : svg 시범 추가)
-=======
->>>>>>> c18953d (✨ Feat: svg 파일 컴포넌트화 설정)
+import AdBanner from "./_components/banner";
+import PerformanceList from "./_components/performance-list";
 
 export default function HomePage() {
   const headerProps = {
@@ -28,16 +19,9 @@ export default function HomePage() {
   }
 
   return (
-<<<<<<< HEAD
     <MainLayout headerProps={headerProps}>
       <AdBanner />
       <PerformanceList />
     </MainLayout>
-=======
-    <section>
-      <h2 className="font-bold text-2xl underline">Homepage</h2>
-      <LoadingSpinner />
-    </section>
->>>>>>> 09c1441 (✨Feat : svg 시범 추가)
   );
 }
