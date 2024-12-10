654e03777539b31b1947e02c5bcd8d5efaea199b
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-12-11 00:30:10 +0900

    ✨ Feat: Icon 컴포넌트 SVGR 사용할 수 있게 변경

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,19 @@
+import ArrowBack from "@/assets/icons/arrow-back.svg";
+import Icon from "@/components/commons/icon";
 import cn from "@/lib/tailwind-cn";
 import { PropsWithChildren } from "react";
 
 export default function Header({ children, className }: PropsWithChildren<{ className?: string }>) {
   return (
-    <header className={cn("w-full h-[56px] bg-white border border-transparent border-b-gray-100", className)}>
+    <header
+      className={cn(
+        "flex items-center w-full h-[48px] px-4 py-2 rounded-lg bg-white border border-transparent border-b-gray-100",
+        className,
+      )}
+    >
+      <Icon alt="arrow-back" className="cursor-pointer">
+        <ArrowBack />
+      </Icon>
       {children}
     </header>
   );
