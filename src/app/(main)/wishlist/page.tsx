b5407ae4f8105dcf4235014f3582f71595f4b94b
Jamong <arozic425@gmail.com>
2024-12-12 09:39:18 +0900

    ✨ Feat: 바텀 시트&모달 제작 (#31)
    * ✨ Feat: Bottom-Sheet 제작
    
    * ✨ Feat: Modal 제작&Bottom-Sheet 수정

--- src/app/(main)/wishlist/page.tsx
+++ src/app/(main)/wishlist/page.tsx
@@ -1,9 +1,12 @@
 import { Search } from "@/assets/svgr-icons";
 import { MainLayout } from "@/components/layout";
+import { useBottomSheet } from "@/contexts";
 import TextLogo from "public/img/logo-text.svg";
 import Wishlist from "./_components/wishlist";
 
 export default function WishlistPage() {
+  const { open, close } = useBottomSheet();
+
   const headerProps = {
     leftIcons: [{ icon: <TextLogo />, path: "/" }],
     rightIcons: [{ icon: <Search />, path: "/search", size: 24 }],
