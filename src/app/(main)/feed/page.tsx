a37fd56c404dd36a560935f359c657b639119a2e
LGY <leegyoung12@gmail.com>
2024-12-12 19:03:30 +0900

    ✨ Feat: 피드 페이지 제작
    - 피드 페이지 제작

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import Pencil from "@/assets/icons/pencil.svg";
 import Search from "@/assets/icons/search.svg";
 import FeedPageList from "@/components/feed/index";
 import { MainLayout } from "@/components/layout";
+import { useState } from "react";
 import TextLogo from "../../../../public/img/logo-text.svg";
 
+interface Feed {
+  feedId: number;
+  profileImage: string;
+  profileName: string;
+  time: string;
+  feedFiles: string[];
+  contents: string;
+  hashTag: string[];
+  likeCount: number;
+  liked: boolean;
+  commentCount: number;
+  isFollowing: false;
+}
+
 export default function FeedPage() {
+  const [filter, setFilter] = useState<"recommended" | "latest">("recommended");
+
   const headerProps = {
     title: "피드",
     leftIcons: [{ icon: <TextLogo />, path: "/" }],
@@ -13,6 +32,47 @@ export default function FeedPage() {
       { icon: <Search />, path: "/search", size: 24 },
     ],
   };
+
+  // 추천순, 최신순 필터링 추가
+
+  const FEED_DATA: Feed[] = [
+    {
+      feedId: 1,
+      profileImage: "https://via.placeholder.com/40",
+      profileName: "김하비",
+      time: "2024-12-06T21:42:31.279Z",
+      feedFiles: [
+        "https://fastly.picsum.photos/id/194/400/400.jpg?hmac=9M4afVM4gcbNrK0jdWxStpZb97YStvTZPDV_zwfSe7c",
+        "https://fastly.picsum.photos/id/556/400/400.jpg?hmac=eIgLNSug_bhkfJKE7PrkeIX3pmn7FfmmxfQlPcgziBQ",
+        "https://fastly.picsum.photos/id/861/400/400.jpg?hmac=Bt3C22W8d4rkkTYLllIRhZyKnD8LLvwgzUmqhGjzKsI",
+      ],
+      contents:
+        "피드 텍스트 기본 텍스트 박스 최대 3줄 그 이상 말 줄임표 + 더보기 버튼 사용 피드 텍스트 기본 텍스트 박스  ",
+      hashTag: ["테스트", "해시태그"],
+      likeCount: 8,
+      liked: false,
+      commentCount: 3,
+      isFollowing: false,
+    },
+    {
+      feedId: 2,
+      profileImage: "https://via.placeholder.com/40",
+      profileName: "김하비",
+      time: "2024-12-06T21:42:31.279Z",
+      feedFiles: [
+        "https://fastly.picsum.photos/id/524/400/400.jpg?hmac=msawrWikHM9c5n3cItDml12lRmNkSrzXlRWyTnoIdig",
+        "https://fastly.picsum.photos/id/456/400/400.jpg?hmac=Wj-1M_DzwOwj84HvgJKFMtgB4xX886nXBuuOIRlK7vk",
+        "https://fastly.picsum.photos/id/334/400/400.jpg?hmac=UpHNsYIHrauzlcGPuvPr_NnVeOBwx-6ivqTCh1_IaYE",
+      ],
+      contents:
+        "피드 텍스트 기본 텍스트 박스 최대 3줄 그 이상 말 줄임표 더보기 버튼 사용 피드 텍스트 기본 텍스트 박스  텍스트 기본 텍스트 박스 텍스트 기본 텍스트 박스 텍스트 기본 텍스트 박스 텍스트 기본 텍스트 피드 텍스트 기본 텍스트 박스 최대 3줄 그 이상 말 줄임표 + 더보기 버튼 사용 피드 텍스트 기본 텍스트 박스  텍스트 기본 텍스트 박스 텍스트 기본 텍스트 박스 텍스트 기본 텍스트 박스 텍스트 기본 텍스트 ",
+      hashTag: ["테스트", "해시태그"],
+      likeCount: 8,
+      liked: false,
+      commentCount: 3,
+      isFollowing: false,
+    },
+  ];
 
   return (
     <MainLayout headerProps={headerProps}>
