f159daf3a8b09468c587a1ac80d455b9ccb9ff15
LGY <leegyoung12@gmail.com>
2024-12-15 01:22:40 +0900

    ♻️ Refactor: 피드 페이지 컴포넌트 분리 및 경로 수정
    - 피드 페이지의 컴포넌트 분리
    - 댓글 페이지, 피드 작성 페이지 경로 수정
    - 모달창 상태 관리를 위한 store 파일 추가
    - feed 관련 타입 파일 폴더 생성 및 이동

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -30,64 +30,9 @@ export default function FeedPage() {
     ],
   };
 
-  const loggedInProfileId = 5; // 로그인 유저의 profileId 설정
-
-  const feedData: Feed[] = [
-    {
-      feedId: 1,
-      contents: "이것은 피드 1내용입니다.",
-      profileName: "sample5",
-      profileId: 5,
-      profileImageUrl: null,
-      hashTag: ["#여행", "#힐링"],
-      feedFiles: ["https://example.com/image1.jpg", "https://example.com/image2.jpg"],
-      likeCount: 0,
-      commentCount: 0,
-      performId: "PF214630",
-      performName: "영재한음 (국악) 회",
-      startDate: "2023.01.29",
-      endDate: "2024.12.29",
-      genreName: "한국음악(국악)",
-      performState: "공연중",
-      placeName: "남산골한옥마을",
-      openRun: false,
-      area: "서울특별시",
-      poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF214630_230308_183127.PNG",
-      performLikeCount: 7,
-      relativeTime: "1일 전",
-      liked: true,
-    },
-    {
-      feedId: 2,
-      contents: "피드 작성 API 테스트입니다.",
-      profileName: "sample2",
-      profileId: 2,
-      profileImageUrl: null,
-      hashTag: ["해시태그", "테스트", "Hobiday"],
-      feedFiles: [
-        "https://hyun-buckbuck.s3.ap-northeast-2.amazonaws.com/test/20241212150510-248bf875-1732-4b19-951f-75db079706a2-EdIWKy6UEAAZl-o.jpg",
-      ],
-      likeCount: 0,
-      commentCount: 0,
-      performId: "PF255055",
-      performName: "원: 죄 [창원]",
-      startDate: "2024.12.03",
-      endDate: "2025.01.26",
-      genreName: "연극",
-      performState: "공연중",
-      placeName: "사랑새아트홀",
-      openRun: false,
-      area: "경상남도",
-      poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF255055_241202_161340.jpg",
-      performLikeCount: 10,
-      relativeTime: "1일 전",
-      liked: false,
-    },
-  ];
-
   return (
     <MainLayout headerProps={headerProps}>
-      <FeedPageList feedData={feedData} loggedInProfileId={loggedInProfileId} />
+      <FeedPageList />
     </MainLayout>
   );
 }
