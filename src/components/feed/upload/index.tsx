78565c3d9bc1e24e19c1189ce6f53965d24849e0
LGY <leegyoung12@gmail.com>
2024-12-16 10:17:27 +0900

    ✨ Feat: Feed 등록 데이터 연동, 이미지 저장 연동

--- src/components/feed/upload/index.tsx
+++ src/components/feed/upload/index.tsx
@@ -6,10 +6,13 @@ import AddInfo from "@/app/(main)/feed/upload/_component/add-info";
 import HashtagInput from "@/app/(main)/feed/upload/_component/hashtag-input";
 import SelectCategory from "@/app/(main)/feed/upload/_component/category";
 import useUploadTextStore from "@/stores/useUploadTextStore";
+import usePresignedURL from "@/hooks/feed/use-image-upload";
+import useFeedRegistration from "@/hooks/feed/use-feed-upload";
 
 export default function UploadPage() {
-  const { performId, content, category, hashTags, fileUrls, setCategory, setHashTags, setFileUrls } =
-    useUploadTextStore();
+  const { performId, content, category, hashTags, photos, setHashTags } = useUploadTextStore();
+  const { uploadImages, isLoading: isUploading } = usePresignedURL();
+  const { registerFeed, isLoading: isRegistering } = useFeedRegistration();
 
   function handleAddHashTags(tags: string[]) {
     setHashTags([...hashTags, ...tags]);
@@ -19,7 +22,30 @@ export default function UploadPage() {
     setHashTags(hashTags.filter((t) => t !== tag));
   }
 
-  // 피드 작성 API 호출
+  async function handleUpload() {
+    if (photos.length === 0) {
+      alert("사진을 업로드해주세요.");
+      return;
+    }
+
+    try {
+      const uploadedUrls = await uploadImages(photos);
+      // url 주소 추가
+      const baseAddrss = process.env.NEXT_PUBLIC_API_BASE_URL;
+      const fullUrls = uploadedUrls.map((url) => `${baseAddrss}${url}`);
+
+      await registerFeed({
+        performId: "PF254874",
+        content,
+        category,
+        hashTags: hashTags.length > 0 ? hashTags : [],
+        fileUrls: fullUrls,
+      });
+      console.log("등록 성공");
+    } catch (err) {
+      console.error("업로드 실패", err);
+    }
+  }
 
   return (
     <div className="bg-white">
@@ -28,6 +54,10 @@ export default function UploadPage() {
       <TextInput />
       <HashtagInput onAddHashTags={handleAddHashTags} onRemoveHashTag={handleRemoveHashTag} />
       <AddInfo />
+
+      <button className="w-full h-12 bg-blue-500 text-white text-lg font-bold" onClick={handleUpload}>
+        사진업로드
+      </button>
     </div>
   );
 }
