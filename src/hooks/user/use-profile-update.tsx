3d16a80f9e51dfa25dd0d5598506f9cdb69ee13c
LGY <leegyoung12@gmail.com>
2024-12-24 14:23:47 +0900

    ♻️ Refactor: 마이페이지 tanstack-query 적용

--- src/hooks/user/use-profile-update.tsx
+++ src/hooks/user/use-profile-update.tsx
@@ -1,6 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { PROFILE_KEYS } from "../queries";
+import { getMyProfile } from "@/apis/user-api";
+import { useUserStore } from "@/stores/useUserStore";
+import React from "react";
 
-const updateProfile = async (data: { [key: string]: string | string[] }) => {
+export const updateProfile = async (data: { [key: string]: string | string[] }) => {
   try {
     const accessToken = localStorage.getItem("accessToken");
     if (!accessToken) {
@@ -21,4 +26,26 @@ const updateProfile = async (data: { [key: string]: string | string[] }) => {
   }
 };
 
-export default updateProfile;
+export const useGetMyProfile = () => {
+  const setUser = useUserStore((state) => state.setUser);
+
+  const queryResult = useQuery({
+    queryKey: [PROFILE_KEYS.myProfile],
+    queryFn: async () => {
+      const data = await getMyProfile();
+      return data;
+    },
+    select: (data) => data || null,
+  });
+
+  // 프로필 데이터 전역 상태에 저장
+  const { data } = queryResult;
+
+  React.useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, setUser]);
+
+  return queryResult;
+};
